29e3a214e71062883e5be94d6ef19304c67cb10c
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-09-03 19:16:34 -0600

    added new string RemoveFirstLast function

--- SharpLearningLab/Main.cs
+++ SharpLearningLab/Main.cs
@@ -1,19 +1,16 @@
 ﻿using System;
 using System.Collections.Immutable;
 
-class SharpLearningExperiments
+class Arrays
 {
-
     public int Sum(int a, int b)
     {
         return a + b;
     }
-
     public static int Area(int a, int b)
     {
         return a * b / 2;
     }
-
     public static string Month(int a)
     {
         // string[] monthArr = new string[12];
@@ -27,7 +24,6 @@ class SharpLearningExperiments
         Array.Sort(values);
         return new double[] { values[0], values[values.Length - 1] };
     }
-
     public static int GetAbsSum(int[] arr)
     {
         int sum = 0;
@@ -39,7 +35,6 @@ class SharpLearningExperiments
 
         return sum;
     }
-
     public static int[] MultiplyByLength(int[] arr)
     {
         int arrLen = arr.Length;
@@ -50,23 +45,54 @@ class SharpLearningExperiments
 
         return arr;
     }
+    public static int[] SortNumsAscending(int[] arr)
+    {
+        if (arr.Length == 0)
+        {
+            int[] emptyArr = new int[] { };
+            return emptyArr;
+        }
+
+         
+        return arr;
+    }
+    public static string GetFilename(string path)
+    {
+        string[] arr = path.Split('/');
+        return arr[arr.Length - 1];
+    }
 }
 
+class Strings
+{
+    public static string RemoveFirstLast(string str)
+    {
+        if (str.Length <= 2)
+        {
+            return str;
+        }
+    return str.Substring(1, str.Length - 2);
+    }
+}
 class MainProg
 {
     public static void Main()
     {
-        var a = new SharpLearningExperiments();
-        var input  = Console.ReadLine();
-        string[] arrInput = input.Split(' ');
-        int[] intArr = arrInput.Select(int.Parse).ToArray();
-        
-        Console.WriteLine($"Sum: {SharpLearningExperiments.GetAbsSum(intArr)}");
-        
+        // var a = new Arrays();
+        string output = Strings.RemoveFirstLast("kd");
+        Console.WriteLine(output);
+
+        // string input  = Console.ReadLine();
+        // string filename = Arrays.GetFilename(input);
+        // Console.WriteLine($"filename: {filename}" );
+
+
+        // Console.WriteLine($"Sum: {SharpLearningExperiments.GetAbsSum(intArr)}");
+
         // Console.WriteLine(Practice.Month(int.Parse(Console.ReadLine())));
         // Console.WriteLine(a.Sum(1, 2));
         // Console.WriteLine(Practice.Area(1, 2));
-        
+
         // int[] arr;
         // int[] arr = new int[12];
 
